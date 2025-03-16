78bf7192fc6ea3f00fb50c7d5b4e9020f9e6910f
setsuna1211 <burai.pkg@gmail.com>
2025-03-16 23:04:03 +0900

    YoutubeAPIをキャッシュするよう変更

--- unofficial_site_niu_yozuna/src/app/api/youtube/list/route.tsx
+++ unofficial_site_niu_yozuna/src/app/api/youtube/list/route.tsx
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
 const BASE_URL = "https://www.googleapis.com/youtube/v3/search";
@@ -17,7 +17,7 @@ interface VideoItem {
   };
 }
 
-export async function GET(req: NextRequest) {
+export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const channelId = searchParams.get("channelId");
 
@@ -31,7 +31,7 @@ export async function GET(req: NextRequest) {
   const url = `${BASE_URL}?part=snippet&channelId=${channelId}&maxResults=6&type=video&order=date&eventType=completed&key=${API_KEY}`;
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, { next: { revalidate: 600 } });
     const data = await response.json();
 
     if (data.error) {
@@ -44,7 +44,7 @@ export async function GET(req: NextRequest) {
       thumbnail: item.snippet.thumbnails.medium.url,
     }));
 
-    return NextResponse.json({ videos }, { status: 200 });
+    return NextResponse.json({ videos });
   } catch {
     return NextResponse.json(
       { error: "Failed to fetch YouTube data" },
